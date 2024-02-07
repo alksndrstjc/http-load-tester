19ecf488bd97768b95926aedb031a129fa8c8278
Aleksandar Stajic <aleksandar-stajic@outlook.com>
2024-02-08 00:03:17 +0100

    step 1 done (make a request with the url from command line)

--- src/main/java/org/alksndrstjc/Main.java
+++ src/main/java/org/alksndrstjc/Main.java
@@ -1,18 +1,33 @@
 package org.alksndrstjc;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        try (HttpClient client = HttpClient.newBuilder().build()) {
+            String arg = args[0];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(new URI(arg))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Response code: " + response.statusCode());
+            System.out.println("Body: " + response.body());
+
+
+        } catch (IndexOutOfBoundsException
+                 | URISyntaxException
+                 | IOException
+                 | InterruptedException e) {
+            System.err.println(e.getMessage());
         }
     }
 }
