e310e6dc414ffa3ae67e7f2fe01d49b28171d234
Aleksandar Stajic <aleksandar-stajic@outlook.com>
2024-02-09 18:28:12 +0100

    Cleaned up parser init.

--- src/main/java/org/alksndrstjc/Main.java
+++ src/main/java/org/alksndrstjc/Main.java
@@ -3,45 +3,26 @@ package org.alksndrstjc;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 import org.alksndrstjc.commands.CLIParameters;
-import org.alksndrstjc.service.RequestBuilder;
-import org.alksndrstjc.service.RequestMaker;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import org.alksndrstjc.commands.CLIParser;
 
 public class Main {
 
     public static void main(String[] args) {
-        CLIParameters cliParameters = new CLIParameters();
-
-        JCommander jc = JCommander.newBuilder()
-                .addObject(cliParameters)
-                .build();
-
-        try (HttpClient client = HttpClient.newBuilder().build()) {
+        CLIParser parser = CLIParser.getInstance();
+        JCommander jc = parser.buildParser();
+        try {
             jc.parse(args);
-            if (cliParameters.help) {
-                jc.usage();
-                return;
-            }
+            CLIParameters params = parser.getParameters();
 
-            RequestMaker maker = new RequestMaker(client);
-            HttpRequest request = new RequestBuilder(cliParameters.url).buildRequest();
-            for (int i = 0; i < cliParameters.numberOfCalls; i++) {
-                HttpResponse<String> response = maker.doRequest(request);
-                System.out.println("Response code: " + response.statusCode());
-                System.out.println("Response body: " + response.body());
+            if (params.help) {
+                jc.usage();
             }
 
         } catch (ParameterException ex) {
             System.err.println(ex.getMessage());
             jc.usage();
-        } catch (URISyntaxException | IOException | InterruptedException e) {
-            System.err.println(e.getMessage());
         }
+
     }
 
 }
---
+++ src/main/java/org/alksndrstjc/commands/CLIParser.java
@@ -0,0 +1,29 @@
+package org.alksndrstjc.commands;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+public class CLIParser {
+
+    private final CLIParameters parameters = new CLIParameters();
+
+    private static CLIParser instance;
+
+    public static CLIParser getInstance() {
+        if (instance == null) instance = new CLIParser();
+        return instance;
+    }
+
+    private CLIParser() {
+    }
+
+    public JCommander buildParser() throws ParameterException {
+        return JCommander.newBuilder()
+                .addObject(parameters)
+                .build();
+    }
+
+    public CLIParameters getParameters() {
+        return parameters;
+    }
+}
