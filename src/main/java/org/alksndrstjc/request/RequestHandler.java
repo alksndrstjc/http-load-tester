47a8afdd7d293c58cc44f15f55e21ad1a38c0767
Aleksandar Stajic <aleksandar-stajic@outlook.com>
2024-03-06 14:38:57 +0100

    Separate connection setup in RequestHandler

--- src/main/java/org/alksndrstjc/request/RequestHandler.java
+++ src/main/java/org/alksndrstjc/request/RequestHandler.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
@@ -31,28 +32,17 @@ public class RequestHandler {
 
     public RequiredData doRequest() {
         try {
-
             HttpURLConnection connection = (HttpURLConnection) new URI(url).toURL().openConnection();
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(2500);
-            // set method
-            connection.setRequestMethod(method != null ? method.getValue() : HttpMethodName.GET.getValue());
-            // set headers
-            if (headers != null) {
-                for (String header : headers) {
-                    String[] split = header.split(":");
-                    connection.setRequestProperty(split[0].trim(), split[1].trim());
-                }
-            }
-            // set body
-            if (body != null && !body.isEmpty()) {
-                connection.setDoOutput(true);
+            setup(connection);
+
+            if (body != null) {
                 try (OutputStream outputStream = connection.getOutputStream()) {
                     outputStream.write(body.getBytes(StandardCharsets.UTF_8));
                 }
             }
 
             long start = System.currentTimeMillis();
+
             long timeToFirstByte = 0;
             long timeToLastByte;
 
@@ -67,7 +57,7 @@ public class RequestHandler {
                 }
                 timeToLastByte = System.currentTimeMillis();
             }
-            connection.getResponseCode();
+
             connection.disconnect();
 
             long end = System.currentTimeMillis();
@@ -87,4 +77,18 @@ public class RequestHandler {
         }
     }
 
+    private void setup(HttpURLConnection connection) throws ProtocolException {
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(2500);
+        connection.setRequestMethod(method != null ? method.getValue() : HttpMethodName.GET.getValue());
+        if (headers != null) {
+            for (String header : headers) {
+                String[] split = header.split(":");
+                connection.setRequestProperty(split[0].trim(), split[1].trim());
+            }
+        }
+        if (body != null && !body.isEmpty()) {
+            connection.setDoOutput(true);
+        }
+    }
 }
